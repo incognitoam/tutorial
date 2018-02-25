aae97d398438b04141fda18045dd512627a7762e
amorph <igor.saenko@gmail.com>
2018-02-24 22:01:28 -0500

    test integration between eclipse and aws codestar

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
  exports.get = function(event, context) {
    var contents = fs.readFileSync("public/index.html");
    context.succeed({
-     statusCode: 200,
+     statusCode: 400,
      body: contents.toString(),
      headers: {'Content-Type': 'text/html'}
    });
