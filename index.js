43f21ec199aba4031011f6f318d1d9570e0945cb
amorph <igor.saenko@gmail.com>
2018-02-25 18:08:49 -0500

    5 in a row: support for matchmaking

--- index.js
+++ index.js
@@ -1,130 +1,200 @@
 'use strict';
-var fs = require('fs');
 
- exports.get = function(event, context) {
-   var contents = fs.readFileSync("public/index.html");
-   context.succeed({
-     statusCode: 200,
-     body: contents.toString(),
-     headers: {'Content-Type': 'text/html'}
-   });
- };
+console.log('Loading function');
 
- 'use strict';
+const doc = require('dynamodb-doc');
 
- console.log('Loading function');
+const dynamo = new doc.DynamoDB();
 
- const doc = require('dynamodb-doc');
+// Handler entry point
+exports.handler = function(event, context, callback) {
+	console.log('Received event:', JSON.stringify(event, null, 2));
 
- const dynamo = new doc.DynamoDB();
+    // standard response
+    const calldone = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+//    trialAndError();
+	var result = processRequest (event, context, calldone);
+
+	//calldone(null, JSON.stringify(result));
+};
+ 
+// Parse the method and call corresponding API handler
+function processRequest (event, context, calldone) {
+    var result = "Unknown request";
+    switch (event.httpMethod) {
+        case 'DELETE':
+            result = "DELETE unsupported";
+            break;
+        case 'GET':
+            result = "GET unsupported";
+            break;
+        case 'POST':
+            result = functionProcessPOST( event.body, calldone );
+            break;
+        case 'PUT':
+            result = "PUT unsupported";
+            break;
+        default:
+            result = "Unknown request";
+    }
+    return result;
+};
 
 
- // Handler entry point
- exports.handler = function(event, context, callback) {
- 	console.log('Received event:', JSON.stringify(event, null, 2));
 
-     // standard response
-     const done = (err, res) => callback(null, {
-         statusCode: err ? '400' : '200',
-         body: err ? err.message : JSON.stringify(res),
-         headers: {
-             'Content-Type': 'application/json',
-         },
-     });
+// TODO: use Environment Variables
+// https://docs.aws.amazon.com/lambda/latest/dg/env_variables.html
+// Simple table model to avoid field naming issues throughout the code
+// there may be a more appropirate way
+var TableModel = 
+{
+    TableName : "tutorialGame",
+        Item:{
+            "game_id"    : "game_id", 
+            "player1"   : "player1", 
+//            "player2"   : "player2",      // removed for now, can't scan for null, only for not exists
+            "board"     : "board",        // binary, flattened array[15][15]
+            "state"     : "state"
+        }
+};
 
- 	var result = processRequest (event, context);
+// Define possible game states, basically an enum 
+// with values being string to store in db
+var GameStates =
+{
+    pending : "pending",
+    inprogress: "inprogress",
+    winner1 : "winner1",
+    winner2 : "winner2",
+    draw : "draw"
+};
 
- 	done(null, JSON.stringify(result));
- };
-  
- function processRequest (event, context) {
-     var result = "Unknown request";
-     switch (event.httpMethod) {
-         case 'DELETE':
-             result = "DELETE unsupported";
-             break;
-         case 'GET':
-             result = "GET unsupported";
-             break;
-         case 'POST':
-             result = "POST unsupported";
-             break;
-         case 'PUT':
-             result = "PUT unsupported";
-             break;
-         default:
-             result = "Unknown request";
-     }
-     return result;
- };
+const BoardSide = 15;
+// TODO: standardise all item names in table
 
- function functionProcessPOST( body )
- {
-     	// MyLambdaFunction logic here
- 	
- 	var table = "tutorialTicTacToeGames";
-     
-     var params = {
-         TableName:table,
-         Item:{
-                 "game_id" : 1, 
-                 "user1" : 1, 
-                 "user2" : 2
-             }
-         };
+// Matchmaking 
+// Current approach is prone to race condition due to it being split into
+// 2 ops: scan for open games and then racing to match to an open game
+// Solution? ConditionalWrite in matchToGame if player 2 is still empty, 
+// otherwise retry matchmaking or create a new game
 
- };
+function functionProcessPOST( body, calldone )
+{
+    var parsed = JSON.parse(body);
+    var player = parsed.name;
+    console.log("functionProcessPOST:", JSON.stringify(body, null, 2));
+    performMatchmaking( player, calldone );
 
- /**
-  * Demonstrates a simple HTTP endpoint using API Gateway. You have full
-  * access to the request and response payload, including headers and
-  * status code.
-  *
-  * To scan a DynamoDB table, make a GET request with the TableName as a
-  * query string parameter. To put, update, or delete an item, make a POST,
-  * PUT, or DELETE request respectively, passing in the payload to the
-  * DynamoDB API as a JSON body.
-  */
-  /*
- exports.handler = (event, context, callback) => {
-     console.log('Received event:', JSON.stringify(event, null, 2));
+};
 
-     const done = (err, res) => callback(null, {
-         statusCode: err ? '400' : '200',
-         body: err ? err.message : JSON.stringify(res),
-         headers: {
-             'Content-Type': 'application/json',
-         },
-     });
-     
-//     done(new Error(`Debug: request received: "${JSON.stringify(event, null, 2)}"`));
-     var table = "tutorialTicTacToeGames";
-     
-     var params = {
-         TableName:table,
-         Item:{
-                 "game_id" : 1, 
-                 "user1" : 1, 
-                 "user2" : 2
-             }
-         }
+function performMatchmaking( playerName, calldone )
+{
+    // Scan if game exists, then matchToGame, otherwise createNewGame
+    var openGame = openGameExists( playerName, createNewGame, matchToGame, calldone );
+}
 
-     switch (event.httpMethod) {
-         case 'DELETE':
-             dynamo.deleteItem(JSON.parse(event.body), done);
-             break;
-         case 'GET':
-             dynamo.scan({ TableName: event.queryStringParameters.TableName }, done);
-             break;
-         case 'POST':
-             dynamo.putItem(params, done);
-             break;
-         case 'PUT':
-             dynamo.updateItem(JSON.parse(event.body), done);
-             break;
-         default:
-             done(new Error(`Unsupported method "${event.httpMethod}"`));
-     }
-     
- };
- */
+
+function openGameExists( playerName, createGame, matchGame, calldone )
+{
+    // scan will get longer the more games are played. 
+    // TODO: solve. e.g. finished games can be moved to a new table?
+    // TODO: replace with query - more efficient and we only need first found.
+    // TODO: check if same player tries to open another game and reject
+    var scanIsGameOpen = {
+        TableName : TableModel.TableName,
+        FilterExpression: `attribute_not_exists(${TableModel.Item.player2})`
+    };
+    
+    dynamo.scan(scanIsGameOpen, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Scan returned:", JSON.stringify(data, null, 2));
+        if( data.Count == 0 )
+        {
+            createGame( playerName, calldone );
+        }else
+        {
+            matchGame( data.Items[0].game_id, playerName, calldone );
+        }
+    }
+    });
+};
+
+function matchToGame( game_id, playerName, calldone )
+{
+    console.log("matchToGame:", `"${game_id}"`);
+    //calldone( new Error(`Not implemented: matchToGame`));
+    
+    var updateRequest = {
+            TableName: TableModel.TableName,
+            Key: {
+                "game_id": game_id
+            },
+            UpdateExpression: "SET player2 = :player2",
+            ExpressionAttributeValues: {
+                ":player2": playerName
+            },
+            ReturnValues: "UPDATED_NEW"
+        };
+
+    dynamo.updateItem(updateRequest, function(err, data) {
+    if (err) {
+        console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Update returned:", JSON.stringify(data, null, 2));
+    }
+    });
+    calldone( null, `Game matched for "${playerName}": "${game_id}"`);
+};
+
+function createNewGame( playerName, calldone )
+{
+    console.log("createNewGame:", JSON.stringify(playerName));
+    
+    // calldone( new Error(`Not implemented: createNewGame`));
+    var createRequest = TableModel;
+    createRequest.Item.game_id = `${Date.now()} - ${playerName}`;// unique game id. TODO: how to use UUID?
+    createRequest.Item.player1 = playerName;
+    //createRequest.Item.player2 = null; // can't scan for null, only for not exists
+    var board = []; board.length = BoardSide*BoardSide;
+    createRequest.Item.board = board;// flatten array[15][15] array of zeroes
+    createRequest.Item.state = GameStates.pending; 
+    
+    console.log("createNewGame:", JSON.stringify(createRequest));
+    
+    dynamo.putItem(createRequest, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Put returned:", JSON.stringify(data, null, 2));
+    }
+    });
+    
+    calldone( null, `Game created for "${playerName}": "${createRequest.Item.gameId}"`);
+};
+
+
+function trialAndError()
+{
+    var req = {
+        TableName: TableModel.TableName,
+        KeyConditionExpression: "player1 = :a",
+        ExpressionAttributeValues: {
+            ":a": "Player X"
+        }
+    };
+    dynamo.query(req, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Scan returned:", JSON.stringify(data, null, 2));
+    }
+    });
+}
